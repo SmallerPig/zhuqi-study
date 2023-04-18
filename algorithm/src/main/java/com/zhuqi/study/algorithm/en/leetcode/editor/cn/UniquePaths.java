d2be03b87e5cd18069d9968810524b290c35f097
zhuqi <zhuqi@lrhealth.com>
2023-04-18 19:23:01 +0800

    feat: question 62 不同的路径 tag= 动态规划

---
+++ algorithm/src/main/java/com/zhuqi/study/algorithm/en/leetcode/editor/cn/UniquePaths.java
@@ -0,0 +1,90 @@
+//一个机器人位于一个 m x n 网格的左上角 （起始点在下图中标记为 “Start” ）。 
+//
+// 机器人每次只能向下或者向右移动一步。机器人试图达到网格的右下角（在下图中标记为 “Finish” ）。 
+//
+// 问总共有多少条不同的路径？ 
+//
+// 
+//
+// 示例 1： 
+// 
+// 
+//输入：m = 3, n = 7
+//输出：28 
+//
+// 示例 2： 
+//
+// 
+//输入：m = 3, n = 2
+//输出：3
+//解释：
+//从左上角开始，总共有 3 条路径可以到达右下角。
+//1. 向右 -> 向下 -> 向下
+//2. 向下 -> 向下 -> 向右
+//3. 向下 -> 向右 -> 向下
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：m = 7, n = 3
+//输出：28
+// 
+//
+// 示例 4： 
+//
+// 
+//输入：m = 3, n = 3
+//输出：6 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= m, n <= 100 
+// 题目数据保证答案小于等于 2 * 10⁹ 
+// 
+//
+// Related Topics 数学 动态规划 组合数学 👍 1742 👎 0
+
+package com.zhuqi.study.algorithm.en.leetcode.editor.cn;
+
+public class UniquePaths {
+    public static void main(String[] args) {
+        Solution solution = new UniquePaths().new Solution();
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        /**
+         * > 2023/04/18 19:21:02
+         * Success:
+         * Runtime:0 ms, faster than 100.00% of Java online submissions.
+         * Memory Usage:38 MB, less than 91.03% of Java online submissions.
+         * 此处的动态规划思路比较简单,
+         *
+         * @param m
+         * @param n
+         * @return
+         */
+        public int uniquePaths(int m, int n) {
+            int[][] dp = new int[m][n];
+            for (int i = 0; i < m; i++) {
+                dp[i][0] = 1;
+            }
+            for (int i = 0; i < n; i++) {
+                dp[0][i] = 1;
+            }
+            for (int i = 1; i < m; i++) {
+                for (int j = 1; j < n; j++) {
+                    dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+                }
+            }
+            return dp[m - 1][n - 1];
+
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
