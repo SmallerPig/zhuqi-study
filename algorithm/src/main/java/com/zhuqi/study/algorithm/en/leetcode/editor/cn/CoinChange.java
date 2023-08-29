956cb4bd29bcf793076feb5aaf6aa1c33b392f81
zhuqi <zhuqiwuxi@gmail.com>
2023-08-29 17:59:37 +0800

    322 零钱兑换 复习,还是不能秒..

--- algorithm/src/main/java/com/zhuqi/study/algorithm/en/leetcode/editor/cn/CoinChange.java
+++ algorithm/src/main/java/com/zhuqi/study/algorithm/en/leetcode/editor/cn/CoinChange.java
@@ -65,26 +65,23 @@ public class CoinChange {
          */
         public int coinChange(int[] coins, int amount) {
             int[] dp = new int[amount + 1];
-            Arrays.fill(dp, amount + 1);
+            Arrays.fill(dp, amount+1);
             dp[0] = 0;
-            for (int i = 1; i <= amount; i++) {
-
+            for (int i = 1; i < amount + 1; i++) {
                 for (int coin : coins) {
-                    if (coin > i) {
+                    if(coin>i){
                         continue;
                     }
-                    if (coin == i) {
-                        dp[coin] = 1;
-                        break;
-                    }
-                    if (dp[i - coin] != Integer.MIN_VALUE) {
-                        dp[i] = Math.min(dp[i - coin] + 1, dp[i]);
+                    if(i-coin==0){
+                        dp[i] = 1;
+                    }else{
+                        dp[i] = Math.min(dp[i], dp[i-coin]+1);
                     }
                 }
-                //System.out.println("dp[i] = " + dp[i]);
-
             }
-            return dp[amount] == amount + 1 ? -1 : dp[amount];
+
+            return dp[amount]== amount+1?-1: dp[amount];
+
         }
     }
 //leetcode submit region end(Prohibit modification and deletion)
